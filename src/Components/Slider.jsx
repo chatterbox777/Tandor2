e5bb419079a545b8fc6223446edd13f1b6e8d1d9
Vasya <chatterbox1995@mail.ru>
2020-06-03 21:06:33 +0300

    5
    made slider works fine

---
+++ src/Components/Slider.jsx
@@ -0,0 +1,43 @@
+//made repository
+
+import React from "react";
+import classList from "./Slider.module.css";
+
+const radioActive = classList.slider__radio__active;
+
+class Slider extends React.Component {
+  state = {};
+
+  render() {
+    return (
+      <div className={classList.slider__container}>
+        <div className={classList.slider__inputs}>
+          {this.props.slides.map((el) => (
+            <input
+              key={el.id}
+              onClick={(e) => this.props.changeActive(e, el.id)}
+              type={el.type}
+              className={el.id === this.props.isActive ? radioActive : null}
+            />
+          ))}
+        </div>
+        <div className={classList.slider__buttons}>
+          <div
+            onClick={() => this.props.prevSlide()}
+            className={classList.slider__button_left}
+          >
+            <p> {"<"} </p>
+          </div>
+          <div
+            onClick={() => this.props.nextSlide()}
+            className={classList.slider__button_right}
+          >
+            <p> {">"}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Slider;
